efb663eba04569fb40dae32ea167a9838d16a596
YoelBuzgalo <yoelbuzgalo@gmail.com>
2023-11-01 11:06:16 -0400

    Activity 9.3

--- activities.py
+++ activities.py
@@ -15,9 +15,23 @@ def fill_array(length):
         unique_array(arr, i)
     return arr
 
+def unique_list(a_list, value):
+    for index in range(len(a_list)):
+        if a_list[index] == value:
+            return
+    a_list.append(value)
+
+def fill_list(length):
+    a_list = []
+    for i in range(length):
+        unique_list(a_list, i)
+    return a_list
+
 def main():
     an_array = timing.time_function(fill_array, 5000)
+    a_list = timing.time_function(fill_list, 5000)
     print(an_array)
+    print(a_list)
 
 if __name__ == "__main__":
     main()
