3d87dbbbbbfc98b6448b9f2f41c961256d43f6c4
YoelBuzgalo <yoelbuzgalo@gmail.com>
2023-11-01 11:17:08 -0400

    Activity 9.5

--- activities.py
+++ activities.py
@@ -27,11 +27,35 @@ def fill_list(length):
         unique_list(a_list, i)
     return a_list
 
+def unique_set(a_set, value):
+    if value not in a_set:
+        a_set.add(value)
+    return
+
+def fill_set(length):
+    a_set = set()
+    for i in range(0, length-1):
+        unique_set(a_set, i)
+    return a_set
+
+def sets():
+    a_set = {111,3,57, 100}
+    print(a_set)
+    a_set.add(50000)
+    print(a_set)
+
+    b_set = set("foo")
+    print(b_set)
+
+
 def main():
-    an_array = timing.time_function(fill_array, 5000)
-    a_list = timing.time_function(fill_list, 5000)
-    print(an_array)
-    print(a_list)
+    # an_array = timing.time_function(fill_array, 5000)
+    # a_list = timing.time_function(fill_list, 5000)
+    # print(an_array)
+    # print(a_list)
+    # sets()
+    a_set = timing.time_function(fill_set, 5000)
+    print(a_set)
 
 if __name__ == "__main__":
     main()
