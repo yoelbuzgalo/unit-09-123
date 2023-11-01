e382c8902adcc518f183d523ce11ba90d1521c10
YoelBuzgalo <yoelbuzgalo@gmail.com>
2023-11-01 10:19:57 -0400

    Mini practicum (Part 2)

--- mini_practicum/practice.py
+++ mini_practicum/practice.py
@@ -17,3 +17,21 @@ def decreasing_comparator(a, b):
     """
     return a >= b
 
+def is_sorted(a_list, comparator=increasing_comparator):
+    for i in range(0,(len(a_list)-1)):
+        if comparator(a_list[i], a_list[i+1]):
+            continue
+        else:
+            return False
+    return True
+
+def main():
+    list1 = [20,10,30]
+    list2 = [30,20,10]
+    print(is_sorted(list1))
+    print(is_sorted(list1, decreasing_comparator))
+    print(is_sorted(list2, increasing_comparator))
+    print(is_sorted(list2, decreasing_comparator))
+
+if __name__ == "__main__":
+    main()
